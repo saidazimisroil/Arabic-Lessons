402ea4647e1c0c5fc2bb87410a3861c389b2c98a
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-04-03 16:25:21 +0500

    Added dictionary1.png Created partials/nextBtn.ejs

--- controllers/lessonsController.js
+++ controllers/lessonsController.js
@@ -1,3 +1,6 @@
 module.exports.dars1_get = (req, res) => {
   res.render("dars1");
 };
+module.exports.dars1_2_get = (req, res) => {
+  res.render("dars1_2");
+};
--- routes/lessonsRoutes.js
+++ routes/lessonsRoutes.js
@@ -4,5 +4,6 @@ const lessonsController = require("../controllers/lessonsController");
 const router = Router();
 
 router.get("/dars1", lessonsController.dars1_get);
+router.get("/dars1-2", lessonsController.dars1_2_get);
 
 module.exports = router;
