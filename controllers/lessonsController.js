f7981339d0430170bf50cc8587ba8705eb461848
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-04-04 10:19:03 +0500

    Completed GET /dars1-3

--- controllers/lessonsController.js
+++ controllers/lessonsController.js
@@ -4,3 +4,10 @@ module.exports.dars1_get = (req, res) => {
 module.exports.dars1_2_get = (req, res) => {
   res.render("dars1_2");
 };
+module.exports.dars1_2_post = (req, res) => {
+  res.redirect("/dars1-3");
+};
+
+module.exports.dars1_3_get = (req, res) => {
+  res.render("dars1_3");
+};
--- routes/lessonsRoutes.js
+++ routes/lessonsRoutes.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/dars1", lessonsController.dars1_get);
 router.get("/dars1-2", lessonsController.dars1_2_get);
+router.post("/dars1-2", lessonsController.dars1_2_post);
+
+router.get("/dars1-3", lessonsController.dars1_3_get);
 
 module.exports = router;
