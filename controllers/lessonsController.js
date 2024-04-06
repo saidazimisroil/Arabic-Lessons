e5afacc87fb7635c919915733640985d1259ec78
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-04-06 08:02:06 +0500

    Completed dars2

--- controllers/lessonsController.js
+++ controllers/lessonsController.js
@@ -1,3 +1,4 @@
+// TODO: dars1 Routes
 module.exports.dars1_get = (req, res) => {
   res.render("dars1");
 };
@@ -10,4 +11,38 @@ module.exports.dars1_2_post = (req, res) => {
 
 module.exports.dars1_3_get = (req, res) => {
   res.render("dars1_3");
+};
+module.exports.dars1_3_post = (req, res) => {
+  res.redirect("/dars2");
+};
+
+// TODO: dars2 Routes
+module.exports.dars2_get = (req, res) => {
+  res.render("dars2_1");
+};
+
+module.exports.dars2_2_get = (req, res) => {
+  res.render("dars2_2");
+};
+module.exports.dars2_2_post = (req, res) => {
+  res.redirect("/dars2-3");
+};
+
+module.exports.dars2_3_get = (req, res) => {
+  res.render("dars2_3");
+};
+module.exports.dars2_3_post = (req, res) => {
+  res.redirect("/dars2-4");
+};
+
+module.exports.dars2_4_get = (req, res) => {
+  res.render("dars2_4");
+};
+module.exports.dars2_4_post = (req, res) => {
+  res.redirect("/dars3");
+};
+
+// TODO: dars3 Routes
+module.exports.dars3_get = (req, res) => {
+  res.render("dars3");
 };
--- routes/lessonsRoutes.js
+++ routes/lessonsRoutes.js
@@ -3,10 +3,28 @@ const lessonsController = require("../controllers/lessonsController");
 
 const router = Router();
 
+// TODO: dars 1
 router.get("/dars1", lessonsController.dars1_get);
+
 router.get("/dars1-2", lessonsController.dars1_2_get);
 router.post("/dars1-2", lessonsController.dars1_2_post);
 
 router.get("/dars1-3", lessonsController.dars1_3_get);
+router.post("/dars1-3", lessonsController.dars1_3_post);
+
+// TODO: dars 2
+router.get("/dars2", lessonsController.dars2_get);
+
+router.get("/dars2-2", lessonsController.dars2_2_get);
+router.post("/dars2-2", lessonsController.dars2_2_post);
+
+router.get("/dars2-3", lessonsController.dars2_3_get);
+router.post("/dars2-3", lessonsController.dars2_3_post);
+
+router.get("/dars2-4", lessonsController.dars2_4_get);
+router.post("/dars2-4", lessonsController.dars2_4_post);
+
+// TODO: dars 3
+router.get("/dars3", lessonsController.dars3_get);
 
 module.exports = router;
